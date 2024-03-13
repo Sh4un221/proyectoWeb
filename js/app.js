9c067e8c5e8f761f48276a40eb7badea5897272f
Sh4un221 <127805282+Sh4un221@users.noreply.github.com>
2024-03-12 22:26:10 -0500

    feat: :construction: The work on the responsive menu is ongoing.
    The entire responsive page for mobile has been implemented, and work has begun on the dropdown menu to bring the project to completion.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 import {} from "./modules/main.js"
+document.addEventListener('DOMContentLoaded', function () {
+    const menuDisplayButton = document.querySelector('.menuDisplay');
+    const menu = document.querySelector('.menu ul');
+  
+    menuDisplayButton.addEventListener('click', function () {
+      menu.classList.toggle('active');
+    });
+  });
+  
