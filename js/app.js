a74c303fbefe6767baf0a844f654d974a228a4e7
Sh4un221 <127805282+Sh4un221@users.noreply.github.com>
2024-03-12 23:05:24 -0500

    feat: :construction: Work on the mobile menu is ongoing.
    I was thinking about using JS to make the menu interactive, but I couldn't figure it out. So, I looked for a different way to do it. I've found the solution, and now what's left is to style the dropdown menu and make some adjustments in the CSS to finish the project.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
 import {} from "./modules/main.js"
-document.addEventListener('DOMContentLoaded', function () {
-    const menuDisplayButton = document.querySelector('.menuDisplay');
-    const menu = document.querySelector('.menu ul');
-  
-    menuDisplayButton.addEventListener('click', function () {
-      menu.classList.toggle('active');
-    });
-  });
-  
+
+
